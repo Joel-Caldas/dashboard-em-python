25eab8ba33b22823cc937a79eed8a8082e5d7b47
Joel Caldas França <127773635+Joel-Caldas@users.noreply.github.com>
2024-11-15 21:23:58 -0300

    Update and rename dashboard.py to app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ st.set_page_config(page_title="Dashboard Imobiliária", layout="wide")
 menu = ["Bairros Mais Procurados pelos Clientes", 
         "Bairros com Maior Número de Imóveis Ofertados", 
         "Desempenho das Mídias", 
+        "Cadastrais de obras de construção civil por Bairros",
         "Interesse por Tipo de Imóvel nos Bairros Mais Procurados"]
 opcao = st.sidebar.selectbox("Escolha uma opção", menu)
 
@@ -21,6 +22,20 @@ def carregar_dados():
 
 # Carregar os dados
 df = carregar_dados()
+df2 = pd.read_excel("imoveis_basecompleta.xlsx", decimal=',')
+df3 = pd.read_excel("base_cno_final.xlsx", decimal=',')
+
+# Limpeza de dados (Tratamento)
+df.drop(['Cliente', 'Telefone(s)', 'Email', '$ Venda', '$ Locação', 
+         'Nome do condomínio/edifício', 'Referências', 'Usuário(s)'], axis=1, inplace=True)
+
+df['Dt. Cadastro'] = pd.to_datetime(df['Dt. Cadastro'])
+df['Data'] = df['Dt. Cadastro'].dt.date
+df.drop('Dt. Cadastro', axis=1, inplace=True)
+
+df['Bairro'] = df['Região'].str.split(',').str[0]
+df.drop(['Região', 'Area'], axis=1, inplace=True)
+df.dropna(inplace=True)
 
 # Título do Dashboard
 st.title("Dashboard - Análise Imobiliária")
@@ -28,14 +43,14 @@ st.title("Dashboard - Análise Imobiliária")
 # Exibir diferentes seções de acordo com a opção escolhida no menu
 if opcao == "Bairros Mais Procurados pelos Clientes":
     st.header("Bairros Mais Procurados pelos Clientes")
-    bairros_procurados = df['Região'].value_counts().head(10)
+    bairros_procurados = df['Bairro'].value_counts().head(10)
     fig1 = px.bar(bairros_procurados, x=bairros_procurados.index, y=bairros_procurados.values, 
                   labels={'x': 'Bairros', 'y': 'Procuras'}, title="Top 10 Bairros Mais Procurados")
     st.plotly_chart(fig1)
 
 elif opcao == "Bairros com Maior Número de Imóveis Ofertados":
     st.header("Bairros com Maior Número de Imóveis Ofertados")
-    bairros_ofertados = df['Região'].value_counts().head(10)
+    bairros_ofertados = df2['Bairro'].value_counts().head(10)
     fig2 = px.bar(bairros_ofertados, x=bairros_ofertados.index, y=bairros_ofertados.values, 
                   labels={'x': 'Bairros', 'y': 'Ofertas'}, title="Top 10 Bairros com Mais Ofertas de Imóveis")
     st.plotly_chart(fig2)
@@ -49,11 +64,23 @@ elif opcao == "Desempenho das Mídias":
                   title="Desempenho das Mídias")
     st.plotly_chart(fig3)
 
+elif opcao == "Cadastrais de obras de construção civil por Bairros (Base Externa O Cadastro Nacional de Obras-CNO)":
+    st.header("Cadastrais de obras de construção civil por Bairros")
+    construcao = df3['Bairro'].value_counts().head(10)
+    fig4 = px.bar(
+        x=construcao.index, 
+        y=construcao.values,
+        labels={'x': 'Bairros', 'y': 'Obras por Bairros'},  # Corrigido: separação das chaves no dicionário de rótulos
+        title="Distribuição de Obras de Construção Civil por Bairros"
+    )
+    st.plotly_chart(fig4)
+
+
 elif opcao == "Interesse por Tipo de Imóvel nos Bairros Mais Procurados":
     st.header("Interesse por Tipo de Imóvel nos Bairros Mais Procurados")
-    bairros_mais_procurados = df['Região'].value_counts().head(10).index
-    df_interesse = df[df['Região'].isin(bairros_mais_procurados)]
-    df_interesse = df_interesse.groupby(['Região', 'Tipo']).size().unstack().fillna(0)
-    fig4 = px.bar(df_interesse, x=df_interesse.index, y=df_interesse.columns, 
+    bairros_mais_procurados = df['Bairro'].value_counts().head(10).index
+    df_interesse = df[df['Bairro'].isin(bairros_mais_procurados)]
+    df_interesse = df_interesse.groupby(['Bairro', 'Tipo']).size().unstack().fillna(0)
+    fig5 = px.bar(df_interesse, x=df_interesse.index, y=df_interesse.columns, 
                   title="Interesse por Tipo de Imóvel nos Bairros Mais Procurados", barmode='stack')
-    st.plotly_chart(fig4)
+    st.plotly_chart(fig5)
